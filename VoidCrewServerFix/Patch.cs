c88ab8c98923c0f1333ef442721e69a0d7377973
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-11-07 20:37:17 -0800

    Add project files.

---
+++ VoidCrewServerFix/Patch.cs
@@ -0,0 +1,17 @@
+﻿using CG.Profile;
+using HarmonyLib;
+using Photon.Pun;
+
+namespace VoidCrewServerFix
+{
+    [HarmonyPatch(typeof(PlayerProfileLoader), "Awake")]
+    internal class Patch
+    {
+        static void Postfix()
+        {
+            var PSO = PhotonNetwork.ServerPortOverrides;
+            PSO.MasterServerPort = 27001;
+            PhotonNetwork.ServerPortOverrides = PSO;
+        }
+    }
+}
